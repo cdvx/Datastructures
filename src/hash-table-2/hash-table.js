e6656b9870e97ab63526d7633c1a43be5cdc513d
cdvx <cedriclusiba@gmail.com>
2020-01-09 14:40:47 +0300

    Test hash function

--- src/hash-table-2/hash-table.js
+++ src/hash-table-2/hash-table.js
@@ -3,8 +3,24 @@
 
 let Hashtable = function(){
     let table = new Array();
+
+    let hash =(string)=> {
+        const H   = 53;
+        let total = 0;
+    
+        for (var i = 0; i < string.length; i++) {
+          total += H * total + string.charCodeAt(i);
+        }
+        total %= table.length;
+        if (total < 1) {
+          table.length -1
+        }
+        return parseInt(total);
+      }
+
     return Object.freeze({
-        table
+        table,
+        hash
     })
 }
 
--- src/hash-table-2/tests/hash-table.spec.js
+++ src/hash-table-2/tests/hash-table.spec.js
@@ -8,4 +8,12 @@ describe('Hashtable Interface tests', ()=>{
         let hashtable = new Hashtable()
         expect(hashtable).to.be.frozen
     })
+    it('Test Hashtable table has no predefined length', ()=>{
+        let hashtable = new Hashtable()
+        expect(hashtable.length).to.be.undefined
+    })
+    it('Test Hashtable table hash function return number from from string keys', ()=>{
+        let hashtable = new HashTable()
+        expect(typeof hashtable.hash('key word')).to.be.equal('number')
+    })
 })
